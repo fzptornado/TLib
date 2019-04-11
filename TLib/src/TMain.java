1a9092d8aae043a4ae757c0f9bb8281fd7b194f7
fzpTornadopt <fariborz.zarepour@gmail.com>
2019-04-11 20:23:27 +0430

    first Step Of TLib With Files in Java

---
+++ TLib/src/TMain.java
@@ -0,0 +1,18 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class TMain {
+
+	
+	public static void main(String[] args) {
+	
+		//work eith files
+		TFileManeger fManager = new TFileManeger("dsds");
+		fManager.AppendRow("salam");
+		fManager.AppendRow("salam");
+		fManager.AppendRow("salam");
+		fManager.AppendRow("sal56156am");
+		
+		
+	}
+}
